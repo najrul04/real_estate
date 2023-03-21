3f291f2b139c494c0a6a622b4d7dca52439b3f69
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2023-03-21 17:15:23 +0600

    preparing propertiesForSale, propertiesForRent

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,9 +33,9 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
       linkName='/search?purpose=for-rent'
       imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
     />
-   {/*  <Flex flexWrap='wrap'>
-      {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
-    </Flex> */}
+    <Flex flexWrap='wrap'>
+      {/* {propertiesForRent.map((property) => <Property property={property} key={property.id} />)} */}
+    </Flex>
     <Banner
       purpose='BUY A HOME'
       title1=' Find, Buy & Own Your'
@@ -46,9 +46,9 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
       linkName='/search?purpose=for-sale'
       imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
     />
-   {/*  <Flex flexWrap='wrap'>
-      {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
-    </Flex> */}
+    <Flex flexWrap='wrap'>
+      {/* {propertiesForSale.map((property) => <Property property={property} key={property.id} />)} */}
+    </Flex>
   </Box>
   )
 }
