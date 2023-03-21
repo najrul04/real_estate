8d63b938c7984f4979c269b796f5ef4916df7c70
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2023-03-21 18:33:07 +0600

    adding layout

---
+++ components/Layout.jsx
@@ -0,0 +1,24 @@
+import { Box } from '@chakra-ui/react';
+import Head from 'next/head';
+
+import Footer from './Footer';
+import Navbar from './Navbar';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <title>Real Estate</title>
+      </Head>
+      <Box maxWidth='1280px' m='auto'>
+        <header>
+          <Navbar />
+        </header>
+        <main>{children}</main>
+        <footer>
+          <Footer />
+        </footer>
+      </Box>
+    </>
+  );
+}
