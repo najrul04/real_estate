ed6f74f231c8c33d9c9e97f28dcda461fac5ea62
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2023-03-22 12:48:54 +0600

    fixing navbar and footer

--- components/ImageScollbar.jsx
+++ components/ImageScollbar.jsx
@@ -2,64 +2,49 @@ import { Box, Flex, Icon } from "@chakra-ui/react";
 import Image from "next/image";
 import { useContext } from "react";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+// import 'react-horizontal-scrolling-menu/dist/styles.css';
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
-
 const LeftArrow = () => {
-    const {scrollPrev} = useContext(VisibilityContext)
+    const { scrollPrev } = useContext(VisibilityContext);
 
-    return(
+    return (
         <Flex justifyContent='center' alignItems='center' marginRight='1'>
-        <Icon
-        as={FaArrowAltCircleLeft}
-        onClick={() => scrollPrev()}
-        fontSize='2xl'
-        cursor='pointer'
-        d={['none','none','none','block']}
-        />
+          <Icon
+            as={FaArrowAltCircleLeft}
+            onClick={() => scrollPrev()}
+            fontSize='2xl'
+            cursor='pointer'
+            d={['none','none','none','block']}
+          />
         </Flex>
-    )
-}
+      );
+    }
 
-const RightArrow  = () => {
-    const {scrollNext} = useContext(VisibilityContext)
+    const RightArrow = () => {
+        const { scrollNext } = useContext(VisibilityContext);
+      
+        return (
+          <Flex justifyContent='center' alignItems='center' marginLeft='1'>
+            <Icon
+              as={FaArrowAltCircleRight}
+              onClick={() => scrollNext()}
+              fontSize='2xl'
+              cursor='pointer'
+              d={['none','none','none','block']}
+          />
+          </Flex>
+        );
+      }
 
-    return(
-        <Flex justifyContent='center' alignItems='center' marginRight='1'>
-        <Icon
-        as={FaArrowAltCircleRight}
-        onClick={() => scrollNext()}
-        fontSize='2xl'
-        cursor='pointer'
-        d={['none','none','none','block']}
-        />
-        </Flex>
-    )
-}
-
-const ImageScollbar = ({data}) => (
+export default function ImageSrollbar({ data }) {
+  return (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{ overflow: 'hidden' }} >
-        {data.map((item) => (
-            <Box
-            key={item.id}
-            width="100%" 
-            maxWidth="910px"
-            itemId={item.id} 
-            overflow='hidden' 
-            p='1'
-            >
-                <Image 
-                placeholder="blur" 
-                alt="image"
-                blurDataURL={item.url}
-                src={item.url}  
-                width={1000} 
-                height={500}  
-                sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px" 
-                />
-            </Box>
-        ))}
+      {data.map((item) => (
+        <Box key={item.id} width='910px' itemId={item.id} overflow='hidden' p='1'>
+          <Image placeholder="blur" blurDataURL={item.url} src={item.url} width={1000} height={500}  sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px" alt="image"/>
+        </Box>
+      ))}
     </ScrollMenu>
-)
-
-export default ImageScollbar;
+  );
+}
